5a20114a7932e9c509de15f2f46e9947b8759f13
Larissa Soares <lari_soares9@hotmail.com>
2020-05-15 19:39:21 -0300

    Criando o projeto e adicionando padroes

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Title } from './style';
+
+const Dashboard: React.FC = () => (
+  <Title>Explore respositórios no Github</Title>
+);
+
+export default Dashboard;
